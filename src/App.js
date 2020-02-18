9f4fd30eb4269f6c5c1a7f956eee8d4c3115db51
nikhil <>
2020-02-18 12:41:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([{ text: "Learn Hooks" }]);  //object datatype
+
+  useEffect(() => {  // same componentDidMount and componentDidUpdate
+    document.title = "You clicked " + count + "times";
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>        
+      <p>You clicked {count} times</p>         
+      <button onClick={() => setCount(count + 1)}>Click me</button>  
+    </div>               //setState , after this , comp.DidMount(here useEffect) invoked 
   );
 }
 
